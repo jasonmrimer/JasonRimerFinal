a6cc615bbcc6d81f8407b3a77ad691d331651fca
Jason M. Rimer <Atlas@Hitos-Nito-Book-of-Air.local>
2014-05-11 15:17:57 +0200

    Initial Commit

---
+++ JasonRimerFinal/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  JasonRimerFinal
+//
+//  Created by Jason M. Rimer on 5/11/14.
+//  Copyright (c) 2014 lovenirds. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
